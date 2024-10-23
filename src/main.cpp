9eab3cd88a505df56c332d554d5c8ab4d95e6c22
nicolas kruger <nicolas.kruger@cwi.com.br>
2024-10-23 13:00:59 -0300

    feat: :sparkles: buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,57 @@
 #include <Arduino.h>
 
+#define NOTE_C4 261
+#define NOTE_D4 294
+#define NOTE_E4 329
+#define NOTE_F4 349
+#define NOTE_G4 392
+#define NOTE_A4 440
+#define NOTE_B4 493
+#define NOTE_C5 523
+
 // const
 const int showPins[] = {D7, D6, D5};
 const int fps = 60;
 const int delayFps = 1000 / fps;
 const int maxShowTime = 20;
+const int buttonPin = D0;
+const int buzzerPin = D1;
 
 // variable
 int countShowTime = 0;
 bool show = false;
+bool activeBuzzer = false;
+
+// Melodia da música de Zelda (introdução)
+const int melody[] = {
+    NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, // Primeiras notas
+    NOTE_B4, NOTE_A4, NOTE_G4, NOTE_A4, // Seguintes notas
+    NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, // Repetição
+    NOTE_B4, NOTE_A4, NOTE_G4, NOTE_E4  // Fim
+};
+
+// Duração de cada nota (em milissegundos)
+const int noteDurations[] = {
+    300, 300, 300, 600, // Duração das primeiras notas
+    300, 300, 300, 600, // Seguintes
+    300, 300, 300, 600, // Repetição
+    300, 300, 300, 600  // Fim
+};
+
+int currentTime = 0;
+const int noteSize = 16;
+int currentNote;
 
 // put function declarations here:
 
 void setUpShow();
 void setUpButton();
+void setUpBuzzer();
+
 void itIsShowTime();
+
+void lightShow();
+void buzzerShow();
 void activeShow();
 
 void setup()
@@ -22,6 +59,7 @@ void setup()
   // put your setup code here, to run once:
   setUpShow();
   setUpButton();
+  setUpBuzzer();
 }
 
 void loop()
@@ -43,12 +81,17 @@ void setUpShow()
   randomSeed(analogRead(0));
 }
 
-void setUpButton()
+void setUpBuzzer()
 {
-  pinMode(D0, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT);
 }
 
-void itIsShowTime()
+void setUpButton()
+{
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void lightShow()
 {
   countShowTime++;
   if (countShowTime < maxShowTime)
@@ -60,6 +103,26 @@ void itIsShowTime()
   {
     digitalWrite(pin, random(2));
   }
+}
+
+void buzzerShow()
+{
+
+  const auto duration = noteDurations[currentNote];
+  if (currentTime >= duration)
+  {
+    currentTime = 0;
+    currentNote = (currentNote + 1) % noteSize;
+  }
+  const auto mel = melody[currentNote];
+  tone(buzzerPin, mel);
+  currentTime += delayFps;
+}
+
+void itIsShowTime()
+{
+  lightShow();
+  buzzerShow();
 }
 
 void activeShow()
